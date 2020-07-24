26f57eb1df95635bf399cd0b1dcba5524370f74f
Hadson Martins <hadsonmartins10@gmail.com>
2020-07-23 22:50:02 -0400

    Update get-promo.js

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -3,16 +3,21 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = value => {
+  const buff = Buffer(value, 'base64');
+  return buff.toString('ascii');
+}
+
 export default async (req, res) => {
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
     await doc.loadInfo()
 
     const sheet = doc.sheetsByIndex[2]
-    await sheet.loadCells('A3:C3')
+    await sheet.loadCells('A3:B3')
 
     const mostrarPromocaoCell = sheet.getCell(2, 0)
     const textoCell = sheet.getCell(2, 1)
